f3f77cbf8b873c18111fb4f5cebe65bfa6e95040
ZharikovaES <z.kate2000@gmail.com>
2022-09-23 01:59:11 +0300

    [0.1] added html-code for sections: header, advantages, routes, start-test, gallery

--- #src/js/main.js
+++ #src/js/main.js
@@ -4,3 +4,21 @@ new DynamicAdapt("max").init();
 
 // header menu on mobiles
 
+// slider of header
+
+const swiperHeader = new Swiper(".slider-header", {
+    direction: "vertical",
+    pagination: {
+      el: ".pagination-header .swiper-pagination",
+      clickable: true,
+    },
+  });
+
+// slider of gallery
+
+const swiperGallery = new Swiper(".slider-gallery", {
+    pagination: {
+      el: ".pagination-gallery .swiper-pagination",
+      clickable: true,
+    },
+  });
